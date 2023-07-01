491aeb6c2f3a0cb7c5b8326bfcd76423feed53f2
elvesbd <elvesbd@yahoo.com>
2023-07-01 16:01:56 -0300

    feat: adding validation in view model fields

--- ViewModels/EditorCategoryViewModel.cs
+++ ViewModels/EditorCategoryViewModel.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WebBlog.ViewModels
 {
     public class EditorCategoryViewModel
     {
+        [Required(ErrorMessage = "O campo name é obrigatório")]
+        [StringLength(40, MinimumLength = 3, ErrorMessage = "O campo name deve conter no mínimo 3 caracteres e no máximo 40 caracteres")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "O campo slug é obrigatório")]
         public string Slug { get; set; }
 
     }
