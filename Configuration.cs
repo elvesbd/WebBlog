9d33229105d82d9ee10af537d88d16afea6ab066
elvesbd <elvesbd@yahoo.com>
2023-07-05 16:47:57 -0300

    feat: add configurations to send email

--- Configuration.cs
+++ Configuration.cs
@@ -4,4 +4,13 @@ public static class Configuration
     public static string JwtKey = "jQ7/OgWOKnMIH8Glc8ak/b+gPLQwE568Js74Dxqi+9c=";
     public static string ApiKeyName = "api_key";
     public static string ApiKey = "curso_api_jkdgauyTrsjsajs==kljd/";
+    public static SmtpConfiguration Smtp = new();
+
+    public class SmtpConfiguration
+    {
+        public string Host { get; set; }
+        public int Port { get; set; } = 25;
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
 }
