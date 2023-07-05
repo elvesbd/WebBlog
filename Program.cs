d5337e49fd5ca64db61368cb3ecaec60428545ed
elvesbd <elvesbd@yahoo.com>
2023-07-05 16:48:53 -0300

    refactor: organizing the main class

--- Program.cs
+++ Program.cs
@@ -6,35 +6,63 @@ using WebBlog.Data;
 using WebBlog.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+ConfigureAuthentication(builder);
+ConfigureMvc(builder);
+ConfigureServices(builder);
 
-builder.Services.AddAuthentication(x =>
+var app = builder.Build();
+LoadConfiguration(app);
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
+
+void LoadConfiguration(WebApplication app)
 {
-    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(x =>
+    Configuration.JwtKey = app.Configuration.GetValue<string>("JwtKey");
+    Configuration.ApiKeyName = app.Configuration.GetValue<string>("ApiKeyName");
+    Configuration.ApiKey = app.Configuration.GetValue<string>("ApiKey");
+
+    var smtp = new Configuration.SmtpConfiguration();
+    app.Configuration.GetSection("Smtp").Bind(smtp);
+    Configuration.Smtp = smtp;
+}
+
+void ConfigureAuthentication(WebApplicationBuilder builder)
 {
-    x.TokenValidationParameters = new TokenValidationParameters
+    var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+
+    builder.Services.AddAuthentication(x =>
     {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(key),
-        ValidateIssuer = false,
-        ValidateAudience = false
-    };
-});
-builder.Services
+        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    }).AddJwtBearer(x =>
+    {
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+}
+
+void ConfigureMvc(WebApplicationBuilder builder)
+{
+    builder.Services
     .AddControllers()
     .ConfigureApiBehaviorOptions(options =>
     {
         options.SuppressModelStateInvalidFilter = true;
     });
-builder.Services.AddDbContext<BlogDataContext>();
-builder.Services.AddTransient<TokenService>(); // sempre cria uma nova instância
-// builder.Services.AddScoped(); Cria apenas uma instância por transação, terminou encerra
-// builder.Services.AddSingleton(); Carrega na primeira vez que é chamado para a memoria e mantém
+}
 
-var app = builder.Build();
-app.UseAuthentication();
-app.UseAuthorization();
-app.MapControllers();
-app.Run();
+void ConfigureServices(WebApplicationBuilder builder)
+{
+    builder.Services.AddDbContext<BlogDataContext>();
+    builder.Services.AddTransient<TokenService>(); // sempre cria uma nova instância
+    // builder.Services.AddScoped(); Cria apenas uma instância por transação, terminou encerra
+    // builder.Services.AddSingleton(); Carrega na primeira vez que é chamado para a memoria e mantém
+}
